55cbb21b27c4e7e0f7af7e72e8279217abbf99d2
dreamcf <84278047+dreamcf@users.noreply.github.com>
2022-05-06 23:49:07 +0800

    Delete spring-05 directory

--- spring-05/src/main/java/com/zhang/spring/bean/BeanUtils.java
+++
@@ -1,8 +0,0 @@
-package com.zhang.spring.bean;
-
-public interface BeanUtils {
-
-    Object getBean(String beanName) throws IllegalAccessException, InstantiationException;
-
-    Object getBean(String beanName, Object... args) throws Throwable;
-}
