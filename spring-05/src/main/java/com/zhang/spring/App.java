e53cd06be0b6ec854fb498f528ac2e6f4160ecf5
dream <3307763375@qq.com>
2022-05-06 20:06:49 +0800

    Revert "handwritten_spring"
    This reverts commit 5bf9d07f5340eb2966c776488dcb791f98944264.
    
    # Conflicts:
    #	spring-09/src/main/java/com/zhang/spring/config/BeanDefinitionScan.java

--- spring-05/src/main/java/com/zhang/spring/App.java
+++ spring-05/src/main/java/com/zhang/spring/App.java
@@ -4,6 +4,7 @@ import com.zhang.spring.entry.UserServiceImpl;
 
 import com.zhang.spring.config.BeanDefinitionScan;
 import com.zhang.spring.annotation.ComponentScan;
+import com.zhang.spring.entry.UserTest;
 import com.zhang.spring.factory.BeanDefinitionFactory;
 
 /**
@@ -16,7 +17,7 @@ public class App {
     public static void main(String[] args) throws Throwable {
         BeanDefinitionFactory beanDefinitionFactory = new BeanDefinitionScan(App.class);
         UserServiceImpl userServiceImpl = (UserServiceImpl) beanDefinitionFactory.getBean("UserServiceImpl");
-        UserServiceImpl userServiceImpl1 = (UserServiceImpl) beanDefinitionFactory.getBean("UserServiceImpl");
+        UserTest userServiceImpl1 = (UserTest) beanDefinitionFactory.getBean("UserTest");
         System.out.println(userServiceImpl.equals(userServiceImpl1));
         System.out.println(userServiceImpl.todo());
     }
